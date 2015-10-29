8de4a529ad3aeb899375cb992e5afb2142233aee
Чорный Ю.А <ChornyiUA@skb.ru>
2015-10-29 16:18:20 +0300

    Initial commit

---
+++ app/src/main/java/com/chornyiua/test1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.chornyiua.test1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolBar();
+
+    }
+
+    private void initToolBar() {
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+
+    }
+
+
+}
